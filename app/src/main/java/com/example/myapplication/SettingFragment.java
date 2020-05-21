b35c39095fbe6a8b988e53152d88469cf4ae107e
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2020-05-21 18:33:44 +0800

    midified

--- app/src/main/java/com/example/myapplication/SettingFragment.java
+++ app/src/main/java/com/example/myapplication/SettingFragment.java
@@ -1,14 +1,28 @@
 package com.example.myapplication;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 
-public class SettingFragment extends PreferenceFragment {
 
+public class SettingFragment extends PreferenceFragment {
+    static private final String keyy = "color";
+    SharedPreferences.OnSharedPreferenceChangeListener preferenceChangeListener;
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        addPreferencesFromResource(R.xml.preferences);
+        addPreferencesFromResource(R.xml.preferenes);
+        preferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+//                System.out.println("share = "+sharedPreferences);
+//                System.out.println("key = "+key);
+                if(key.equals(keyy)){
+
+                }
+            }
+        };
     }
 }
